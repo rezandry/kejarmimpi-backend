c1b2865778894fcc5051043246d14df456e08ac7
ervinismu <social.ervin@gmail.com>
2017-09-03 08:58:20 +0700

    add folder project

---
+++ api-kejarmimpi/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "ervinismu/kejarmimpi-backend/api-kejarmimpi/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
